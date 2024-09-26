defff048bffeb619d4b977b4eef6c8f80381f4f2
Latrell Clark <clarklat000@warren.k12.in.us>
2024-09-26 08:08:26 -0400

    in

---
+++ main.py
@@ -0,0 +1,9 @@
+name = input("What is your name: ")
+
+# Input statement for number of droids
+droids = input("How many droids do you want to meet? ")
+
+# Input statement for number of Wookiees
+wookiees = input("How many Wookiees do you want to meet? ")
+
+print(name, "wants to meet", droids, "droids and", wookiees, "Wookiees!")
